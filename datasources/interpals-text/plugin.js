d016140a152de11cc477408c04a44ebd828f4e33
Rory Byrne <roryofbyrne@gmail.com>
2018-03-25 18:32:03 +0000

    Initial commit for interpals-text datasource

---
+++ datasources/interpals-text/plugin.js
@@ -0,0 +1,22 @@
+const registerEventsHandler = function(mc, body) {
+  let textarea = body.getElementById("message");
+  let sendButton = body.getElementById("msg_submit");
+
+  sendButton.addEventListener("click", function(event) {
+    sendDatapoint(textarea.innerHTML, mc)
+  });
+}
+
+const sendDatapoint = function(message, mc) {
+  let datapoint = {};
+  datapoint['message'] = message;
+  datapoint['timestamp'] = Date.now();
+  console.log(datapoint);
+
+  mc.sendDatapoint(datapoint);
+}
+
+function initDataSource(metroClient) {
+  registerEventsHandler(document.Body, metroClient);
+  console.log("Loaded Interpals-Text DataSource");
+}
